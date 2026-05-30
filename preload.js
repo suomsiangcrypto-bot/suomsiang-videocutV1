1562d3eb4a47414e6430e97555cb0b889c73374a
suomsiang <suomsiangcrypto@gmail.com>
2026-05-30 13:22:25 +0700

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,16 +1,1 @@
-const { contextBridge, ipcRenderer } = require('electron');
 
-// expose API ปลอดภัยให้ renderer ใช้งาน
-contextBridge.exposeInMainWorld('electronAPI', {
-  // บันทึกไฟล์ผ่าน native dialog
-  saveFile: (filename, buffer) =>
-    ipcRenderer.invoke('save-file', { filename, buffer }),
-
-  // เปิด folder ใน Explorer/Finder
-  showFile: (filePath) =>
-    ipcRenderer.invoke('show-file', { filePath }),
-
-  // บอก renderer ว่าอยู่ใน Electron
-  isElectron: true,
-  platform: process.platform,
-});
